05a2801902661c901d140ef96b6c985a43b8bce4
User1 <arjun@eamandloi@eworkplaceapps.com>
2018-06-14 18:56:43 +0530

    captcha

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  randomstring = '';
+  @ViewChild('myCanvas') myCanvas;
+
+  ngOnInit() {
+
+    this. getRandomStringForCaptcha();
+    this.customCaptcha(this.randomstring);
+  }
+
+  customCaptcha(string){
+    let c = this.myCanvas.nativeElement;
+    let ctx = c.getContext("2d");
+    ctx.font = "15px Arial";
+    ctx.clearRect(0, 0, 252, 144);
+    ctx.fillStyle = "black";
+    ctx.fillText(string, 15, 21);
+  }
+
+  getRandomStringForCaptcha(){
+      let chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+      let string_length = 4;
+      for (var i=0; i<string_length; i++) {
+        let rnum = Math.floor(Math.random() * chars.length);
+        this.randomstring += chars.substring(rnum,rnum+1);
+      }
+  }
+
+  changeCaptcha(){
+    this.randomstring = '';
+    this.getRandomStringForCaptcha();
+    this.customCaptcha(this.randomstring);
+  }
+
 }
